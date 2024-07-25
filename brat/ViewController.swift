29090d7f20c0d7feb4f3263af3eee6a46b773d00
ChiefTacoOfficer <nathan@ojje.com>
2024-07-25 13:12:03 -0600

    Initial Commit

---
+++ brat/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  brat
+//
+//  Created by Nathan Fennel on 7/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
